8ef1900315412e4e8c2affebf3f00e73209291d6
Pavel Tsikhanau <tishka1991@gmail.com>
2024-06-10 06:41:00 +0300

    feat: add image & year requests

--- scripts/response.js
+++ scripts/response.js
@@ -1,44 +1,50 @@
 import { MovieCard, MoviesList } from './index.js';
 
-const moviesListURL = new URL('https://api.themoviedb.org/3/discover/movie');
+let configurations;
 
-moviesListURL.searchParams.set('page', '4');
-moviesListURL.searchParams.set('with_genres', '878');
-
-const headers = {
-  Authorization: '',
-  accept: 'application/json',
-};
-
-fetch(moviesListURL, {
-  headers,
-  method: 'GET',
-})
-  .then((response) => {
+const makeHttpCall = (url, method = 'GET') => {
+  return fetch(url, {
+    headers: {
+      Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NzkzOWZkN2EzYWYwYTgyN2M2YmI3YjYxMTllOTU0YiIsInN1YiI6IjY2NGIzODNiMjdjMGU2OTMxOWVkM2UzZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AUwDGglMTVwT9-OYHUH4Qrml_zReXFXKf01A5lr4ICc',
+      accept: 'application/json',
+    },
+    method,
+  }).then((response) => {
     if (!response.ok) {
       throw new Error(`error: ${response.statusText}`);
     }
     return response.json();
+  });
+};
+
+//images response
+makeHttpCall('https://api.themoviedb.org/3/configuration')
+  .then((configuration) => {
+    configurations = configuration;
+
+    const moviesListURL = new URL(
+      'https://api.themoviedb.org/3/discover/movie'
+    );
+
+    moviesListURL.searchParams.set('page', '4');
+    moviesListURL.searchParams.set('with_genres', '878');
+
+    return makeHttpCall(moviesListURL);
   })
-  .then((body) => {
-    const movieCardPromises = body.results.map((movie) => {
-      return fetch(`https://api.themoviedb.org/3/movie/${movie.id}`, {
-        headers,
-        method: 'GET',
-      })
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error(`error: ${response.statusText}`);
-          }
-          return response.json();
-        })
-        .then((body) => {
-          const genres = body.genres.map((genre) => genre.name);
+  .then((moviesListJson) => {
+    const movieCardPromises = moviesListJson.results.map((movieShortInfo) => {
+      return makeHttpCall(`https://api.themoviedb.org/3/movie/${movieShortInfo.id}`)
+        .then((movieFullInfo) => {
+          const genres = movieFullInfo.genres.map((genre) => genre.name);
+          const posterUrl = `${configurations.images.base_url}${configurations.images.poster_sizes[6]}${movieFullInfo.poster_path}`;
+          const year = new Date(movieFullInfo.release_date).getFullYear()
           return new MovieCard({
-            posterUrl: body.poster_path,
-            title: body.original_title,
+            posterUrl,
+            title: movieFullInfo.original_title,
+            year,
             genres,
-            ranking: body.vote_average,
+            ranking: movieFullInfo.vote_average,
           });
         });
     });
@@ -46,7 +52,6 @@ fetch(moviesListURL, {
       const moviesList = new MoviesList(movieCards);
       document.querySelector('body').append(moviesList.getHtml());
     });
-    console.log(body.results);
   })
   .catch((error) => {
     console.error(error);
